e2b6ef2588cbdd505b72fe2bc5ed7abfbc0544ab
ryan-son <ryan.son1002@gmail.com>
2021-03-15 18:25:57 +0900

    Fix: UI요소 Layout 적용, Test: 버튼 테스트용 코드 추가

--- JuiceMaker/JuiceMaker/ViewController.swift
+++ JuiceMaker/JuiceMaker/ViewController.swift
@@ -7,12 +7,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+  
+  @IBAction func touchUpOrderStrawberryButton(_ sender: UIButton) {
+    print("I just touched up")
+  }
+  
+  
 }
 
