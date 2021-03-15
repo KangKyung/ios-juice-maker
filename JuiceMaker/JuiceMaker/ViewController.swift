d729704aef07760ed91b55d350f15c04fafe35aa
ryan-son <ryan.son1002@gmail.com>
2021-03-15 22:25:15 +0900

    Feat: 잔여수량 표시 기능 추가 Fix: 우측버튼 버그 픽스
    - juiceMaker.stock.count(for:) 메서드 이용하여 잔여수량 레이블 표시 기능 구현
    - 망고, 키위, 망고키위쥬스 버튼에 touch up inside 적용되어 있지 않은 문제 수정

--- JuiceMaker/JuiceMaker/ViewController.swift
+++ JuiceMaker/JuiceMaker/ViewController.swift
@@ -8,6 +8,7 @@ import UIKit
 
 class ViewController: UIViewController {
   
+  let juiceMaker = JuiceMaker()
   @IBOutlet weak var strawberryJuiceOrderButton: UIButton!
   @IBOutlet weak var bananaJuiceOrderButton: UIButton!
   @IBOutlet weak var kiwiJuiceOrderButton: UIButton!
@@ -15,9 +16,11 @@ class ViewController: UIViewController {
   @IBOutlet weak var strawberryBananaJuiceOrderButton: UIButton!
   @IBOutlet weak var mangoJuiceOrderButton: UIButton!
   @IBOutlet weak var mangoKiwiJuiceOrderButton: UIButton!
-  
-  
-  let juiceMaker = JuiceMaker()
+  @IBOutlet weak var remainingStrawberryNumberLabel: UILabel!
+  @IBOutlet weak var remainingBananaNumberLabel: UILabel!
+  @IBOutlet weak var remainingKiwiNumberLabel: UILabel!
+  @IBOutlet weak var remainingPineappleNumberLabel: UILabel!
+  @IBOutlet weak var remainingMangoNumberLabel: UILabel!
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -28,35 +31,66 @@ class ViewController: UIViewController {
     strawberryBananaJuiceOrderButton.titleLabel?.textAlignment = .center
     mangoJuiceOrderButton.titleLabel?.textAlignment = .center
     mangoKiwiJuiceOrderButton.titleLabel?.textAlignment = .center
+
+    do {
+      remainingStrawberryNumberLabel.text = try String(
+        juiceMaker.stock.count(for: .strawberry))
+      remainingBananaNumberLabel.text = try String(
+        juiceMaker.stock.count(for: .banana))
+      remainingPineappleNumberLabel.text = try String(
+        juiceMaker.stock.count(for: .pineapple))
+      remainingKiwiNumberLabel.text = try String(
+        juiceMaker.stock.count(for: .kiwi))
+      remainingMangoNumberLabel.text = try String(
+        juiceMaker.stock.count(for: .mango))
+    } catch {
+      print("String 타입캐스팅에 실패하였습니다.")
+    }
     // Do any additional setup after loading the view.
   }
   
   @IBAction func touchUpOrderButton(_ sender: UIButton) {
-    switch sender {
-    case strawberryJuiceOrderButton:
-      juiceMaker.make(of: .strawberryJuice)
-    case bananaJuiceOrderButton:
-      juiceMaker.make(of: .bananaJuice)
-    case kiwiJuiceOrderButton:
-      juiceMaker.make(of: .kiwiJuice)
-    case pineappleJuiceOrderButton:
-      juiceMaker.make(of: .pineappleJuice)
-    case strawberryBananaJuiceOrderButton:
-      juiceMaker.make(of: .strawberryBananaJuice)
-    case mangoJuiceOrderButton:
-      juiceMaker.make(of: .mangoJuice)
-    case mangoKiwiJuiceOrderButton:
-      juiceMaker.make(of: .mangoKiwiJuice)
-    default:
-      print("듣도 보도 못한 버튼이군요..")
-      informErrorLocation(functionName: #function)
+    do {
+      switch sender {
+      case strawberryJuiceOrderButton:
+        juiceMaker.make(of: .strawberryJuice)
+        remainingStrawberryNumberLabel.text = try String(
+          juiceMaker.stock.count(for: .strawberry))
+      case bananaJuiceOrderButton:
+        juiceMaker.make(of: .bananaJuice)
+        remainingBananaNumberLabel.text = try String(
+          juiceMaker.stock.count(for: .banana))
+      case kiwiJuiceOrderButton:
+        juiceMaker.make(of: .kiwiJuice)
+        remainingKiwiNumberLabel.text = try String(
+          juiceMaker.stock.count(for: .kiwi))
+      case pineappleJuiceOrderButton:
+        juiceMaker.make(of: .pineappleJuice)
+        remainingPineappleNumberLabel.text = try String(
+          juiceMaker.stock.count(for: .pineapple))
+      case strawberryBananaJuiceOrderButton:
+        juiceMaker.make(of: .strawberryBananaJuice)
+        remainingStrawberryNumberLabel.text = try String(
+          juiceMaker.stock.count(for: .strawberry))
+        remainingBananaNumberLabel.text = try String(
+          juiceMaker.stock.count(for: .banana))
+      case mangoJuiceOrderButton:
+        juiceMaker.make(of: .mangoJuice)
+        remainingMangoNumberLabel.text = try String(
+          juiceMaker.stock.count(for: .mango))
+      case mangoKiwiJuiceOrderButton:
+        juiceMaker.make(of: .mangoKiwiJuice)
+        remainingMangoNumberLabel.text = try String(
+          juiceMaker.stock.count(for: .mango))
+        remainingKiwiNumberLabel.text = try String(
+          juiceMaker.stock.count(for: .kiwi))
+      default:
+        print("듣도 보도 못한 버튼이군요..")
+        informErrorLocation(functionName: #function)
+      }
+    } catch {
+      print("String 타입캐스팅 실패")
     }
-    // 재고확인용 테스트 코드
-    juiceMaker.stock.checkStock(for: .strawberry)
-    juiceMaker.stock.checkStock(for: .banana)
-    juiceMaker.stock.checkStock(for: .mango)
-    juiceMaker.stock.checkStock(for: .pineapple)
-    juiceMaker.stock.checkStock(for: .kiwi)
   }
 }
 
